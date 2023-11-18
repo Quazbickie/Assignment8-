e6e2bfe5f10206b057bb9eba52e7f8ed8fad680b
Ryan <ryankane180@gmail.com>
2023-11-18 19:36:27 +0000

    Animal class complete

---
+++ Assignment8/src/Animal.java
@@ -0,0 +1,57 @@
+import java.util.Objects;
+
+public class Animal {
+
+    private String name;
+    private String breed;
+    private int size;
+
+    public Animal(){
+        this("default","default",0);
+    }
+
+    public Animal(String name, String breed, int size){
+        this.name = name;
+        this.breed = breed;
+        this.size = size;
+    }
+
+    public int compareTo(Animal animal){
+        if(Objects.equals(this.name, animal.name)){
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setBreed(String breed){
+        this.breed = breed;
+    }
+
+    public String getBreed(){
+        return this.breed;
+    }
+
+    public void setSize(int size){
+        this.size = size;
+    }
+
+    public int getSize(){
+        return this.size;
+    }
+
+    public String toString(){
+        return "" + this.name + " is a " + this.breed + " of size: " + this.size;
+    }
+
+
+
+}
